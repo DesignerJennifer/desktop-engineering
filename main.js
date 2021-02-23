e4e429dc5a8d60265bbd9b9fbb4105713a89bb3d
kontraux <69282807+kontraux@users.noreply.github.com>
2021-02-23 17:45:21 -0600

    altered the nav and added table of contents

---
+++ main.js
@@ -0,0 +1,17 @@
+$("navigation.html").ready(function () {
+    $("#navbar").replaceWith(data); });
+
+
+
+
+// let nav = document.getElementsByID="nav";
+
+// const navText = ;
+
+// function injectNav() {
+//     nav.innerHTML = navText;
+
+// }
+
+// injectNav();
+
